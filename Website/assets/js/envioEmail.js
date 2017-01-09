1d6c95720384771b94a111fd8c904b3a506574d2
tialleszerwes <tialles_zerwes@hotmail.com>
2017-01-09 19:23:29 -0200

    Alteração do layout;
    Alteração do layout das imagens para 100%; Criação de método para
    validar o formulario de envio de e-mail; alteração do layout da index
    para aceitar as mensagens de erro;

--- Website/assets/js/envioEmail.js
+++ Website/assets/js/envioEmail.js
@@ -1,12 +1,108 @@
-
-function enviaEmail()
+function enviaForm()
 {
+    debugger;
+    if(validaForm())
+    {
+        //TODO: enviar form do e-mail
+    }
+    else
+    {
+        return false;
+    }
+}
 
+function validaForm()
+{
+    var indValid = true;
+    //valida nome
+     if(document.getElementById("nomeTxt").value.length > 2)
+     {
+     var errorTag = document.getElementById("errorNome");
+            if(errorTag != null){
+                errorTag.remove();
+            }   
+    }
+    else if(document.getElementById("errorNome") == null)
+    {
+         document.getElementById("nomeDiv").innerHTML += "<small> <font id='errorNome' color='red'>Nome inválido </font> </small>";
+            indValid = false;
+    }
+
+    //valida email de envio
+     if(document.getElementById("emailSelect").value != "")
+     {
+            var errorTag = document.getElementById("errorEmail");
+            if(errorTag != null){
+                errorTag.remove();             
+            }   
+     }
+     else if(document.getElementById("errorEmail") == null)
+     {
+          document.getElementById("emailDiv").innerHTML += "<small> <font id='errorEmail' color='red'>E-mail inválido </font> </smal>";
+            indValid = false;
+     }    
+
+     //valida mensagem
+     if(document.getElementById("mensagemTxt").value >= 10)
+     {
+            var errorTag = document.getElementById("errorMensagem");
+            if(errorTag != null){
+                errorTag.remove();               
+            }   
+     }
+     else if(document.getElementById("errorMensagem") == null)
+     {
+          document.getElementById("mensagemDiv").innerHTML += "<small> <font id='errorMensagem' color='red'>Mensagem inválida </font> </small>";
+            indValid = false;
+     }   
+
+     //valida email cliente
+     indValid = validaEmail();
+
+        return indValid;
+}
+
+function nomeChangeWithError()
+{
+    if(document.getElementById("errorNome") != null)
+    {
+        if(document.getElementById("nomeTxt").value.length > 2)
+        {
+            document.getElementById("errorNome").innerHTML = ""; 
+        }
+    }
+}
+
+function emailChangeWithError()
+{
+    if(document.getElementById("errorEmail") != null)
+    {
+        if(document.getElementById("emailSelect").value != "")
+        {
+            document.getElementById("errorEmail").innerHTML = ""; 
+        }
+    }    
+}
+
+function mensagemClienteChangeWithError()
+{
+    if(document.getElementById("errorMensagem") != null)
+    {
+        if(document.getElementById("mensagemTxt").value.length >= 10)
+        {
+            document.getElementById("errorMensagem").innerHTML = ""; 
+        }
+    }
+}
+
+function emailClienteChangeWithError()
+{
+    validaEmail();
 }
 
 function validaEmail()
 {
-    var emailtxt = document.getElementById("emailtxt").value;
+var emailtxt = document.getElementById("emailtxt").value;
 
     usuario = emailtxt.substring(0, emailtxt.indexOf("@"));
     dominio = emailtxt.substring(emailtxt.indexOf("@")+ 1, emailtxt.length);
@@ -21,12 +117,16 @@ function validaEmail()
         (dominio.indexOf(".") >=1)&& 
         (dominio.lastIndexOf(".") < dominio.length - 1)) 
         {
-            document.getElementById("msgemail").innerHTML="E-mail válido";
-            alert("E-mail valido");
+            var errorTag = document.getElementById("errorEmailCliente");
+            if(errorTag != null){
+                errorTag.remove();
+            }
         }
-        else
+        else if(document.getElementById("errorEmailCliente") == null)
         {
-            document.getElementById("msgemail").innerHTML="<font color='red'>E-mail inválido </font>";
-            alert("E-mail invalido");
+            document.getElementById("emailClienteDiv").innerHTML += "<small> <font id='errorEmailCliente' color='red'>E-mail inválido </font> </small>";
+            return false;
         }
+
+        return true;
 }
